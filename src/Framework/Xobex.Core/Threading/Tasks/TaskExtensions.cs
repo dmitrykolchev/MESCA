8dd4cfd28bdc49ed889ff3064160061174870044
Dmitry Kolchev <dmitrykolchev@msn.com>
2023-12-04 11:07:09 +0300

    fix namespace

--- src/Framework/Xobex.Core/Threading/Tasks/TaskExtensions.cs
+++ src/Framework/Xobex.Core/Threading/Tasks/TaskExtensions.cs
@@ -3,7 +3,7 @@
 // See LICENSE in the project root for license information
 // </copyright>
 
-namespace Xobex.Data.Threading.Tasks;
+namespace Xobex.Threading.Tasks;
 
 public static class TaskExtensions
 {
