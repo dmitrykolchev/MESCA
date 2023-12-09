1d38df290a616aedab716b03dfff1aedc84f898f
Dmitry Kolchev <dmitrykolchev@msn.com>
2023-12-09 04:49:38 +0300

    fix layout

--- src/Components/Xobex.Web.Components/Controllers/HomeController.cs
+++ src/Components/Xobex.Web.Components/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Elevation()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
